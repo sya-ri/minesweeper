748cde9ef1b776f79307365b5286f80b2683379a
sya-ri <git@s7a.dev>
2022-05-24 11:05:48 +0900

    chore: Minify html build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
-  plugins: [react()]
+  plugins: [react(), createHtmlPlugin({ minify: true })]
 })
