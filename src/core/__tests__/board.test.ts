69b65a2c18ed4e3b0a7cc46527d7e5b03522c248
sya-ri <git@s7a.dev>
2022-06-14 15:55:06 +0900

    test: Add more expects

--- src/core/__tests__/board.test.ts
+++ src/core/__tests__/board.test.ts
@@ -29,7 +29,9 @@ it('should get the around tiles', () => {
     [new Tile(3), new Tile(4), new Tile(5)],
     [new Tile(6), new Tile(7), new Tile(8)]
   ])
-  expect(board3x3.getAroundTiles(0, 0)).toEqual(expect.arrayContaining([new Tile(1), new Tile(3), new Tile(4)]))
+  expect(board3x3.getAroundTiles(0, 0).map((t) => t.id)).toEqual(expect.arrayContaining([1, 3, 4]))
+  expect(board3x3.getAroundTiles(0, 1).map((t) => t.id)).toEqual(expect.arrayContaining([0, 1, 4, 6, 7]))
+  expect(board3x3.getAroundTiles(1, 1).map((t) => t.id)).toEqual(expect.arrayContaining([0, 1, 2, 3, 5, 6, 7, 8]))
 })
 
 it('should get empty around, for the blank tiles', () => {
