43d88c5df7ff3dbc2bbe49e0518345e0ffa64d1e
sya-ri <git@s7a.dev>
2022-06-16 13:21:43 +0900

    feat: Add generateBoard

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import React, { FC, useState } from 'react'
-import Board from './core/Board'
 import OpenTileResult from './core/tile/OpenTileResult'
-import Tile from './core/tile/Tile'
-
-const tile = (x: number, y: number) => new Tile(x, y)
-const bomb = (x: number, y: number) => new Tile(x, y, true)
+import generateBoard from './core/generateBoard'
 
 const App: FC = () => {
-  const [board, setBoard] = useState(
-    new Board([
-      [bomb(0, 0), bomb(1, 0), tile(2, 0), tile(3, 0), tile(4, 0)],
-      [tile(0, 1), tile(1, 1), bomb(2, 1), bomb(3, 1), bomb(4, 1)],
-      [tile(0, 2), bomb(1, 2), tile(2, 2), tile(3, 2), bomb(4, 2)],
-      [bomb(0, 3), tile(1, 3), tile(2, 3), tile(3, 3), tile(4, 3)],
-      [tile(0, 4), bomb(1, 4), tile(2, 4), tile(3, 4), tile(4, 4)]
-    ])
-  )
+  const [board, setBoard] = useState(() => generateBoard(30, 16, 99))
   return (
     <div>
       {board.tiles.map((tt) => (
---
+++ src/core/generateBoard.ts
@@ -0,0 +1,22 @@
+import Board from './Board'
+import Tile from './tile/Tile'
+
+const generateBoard = (width: number, height: number, numberOfBomb: number): Board => {
+  const bombs = new Set<number>()
+  while (bombs.size < numberOfBomb) {
+    const x = Math.floor(Math.random() * width)
+    const y = Math.floor(Math.random() * height)
+    bombs.add(x * height + y)
+  }
+  const tiles: Tile[][] = []
+  for (let y = 0; y < height; y += 1) {
+    const tt: Tile[] = []
+    for (let x = 0; x < width; x += 1) {
+      tt.push(new Tile(x, y, bombs.has(x * height + y)))
+    }
+    tiles.push(tt)
+  }
+  return new Board(tiles)
+}
+
+export default generateBoard
