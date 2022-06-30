6f2ab2eee628f488892fa31004aa9eea80a8de53
sya-ri <git@s7a.dev>
2022-06-30 20:03:59 +0900

    fix(web): Change not to hide overflow

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState } from 'react'
-import { Box, Button, Flex } from '@chakra-ui/react'
+import { Box, Button, Center, Flex } from '@chakra-ui/react'
 import OpenTileResult from './core/tile/OpenTileResult'
 import Solver from './core/solver/Solver'
 import Board from './core/board/Board'
@@ -9,29 +9,33 @@ import TileButton from './components/TileButton'
 const App: FC = () => {
   const [board, setBoard] = useState<Board>(new Board(new RandomTilesGenerator(30, 16, 99, true)))
   return (
-    <Box mt={2}>
-      {board.tiles.map((tt) => (
-        <Flex justify="center">
-          {tt.map((tile) => (
-            <TileButton
-              tile={tile}
-              countAroundBomb={() => board.countAroundBomb(tile.x, tile.y)}
-              openTile={() => {
-                const result = board.openTile(tile.x, tile.y)
-                if (result === OpenTileResult.GameOver) {
-                  alert('Game Over') // eslint-disable-line no-alert
-                  window.location.reload()
-                }
-                setBoard(board.clone())
-              }}
-              toggleFlag={() => {
-                board.toggleFlag(tile.x, tile.y)
-                setBoard(board.clone())
-              }}
-            />
+    <Box m={2}>
+      <Center overflow="auto">
+        <Box maxW="full">
+          {board.tiles.map((tt) => (
+            <Flex>
+              {tt.map((tile) => (
+                <TileButton
+                  tile={tile}
+                  countAroundBomb={() => board.countAroundBomb(tile.x, tile.y)}
+                  openTile={() => {
+                    const result = board.openTile(tile.x, tile.y)
+                    if (result === OpenTileResult.GameOver) {
+                      alert('Game Over') // eslint-disable-line no-alert
+                      window.location.reload()
+                    }
+                    setBoard(board.clone())
+                  }}
+                  toggleFlag={() => {
+                    board.toggleFlag(tile.x, tile.y)
+                    setBoard(board.clone())
+                  }}
+                />
+              ))}
+            </Flex>
           ))}
-        </Flex>
-      ))}
+        </Box>
+      </Center>
       <Flex m={2} justify="center" gap={1}>
         <Button
           onClick={() => {
