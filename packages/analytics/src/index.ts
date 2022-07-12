90deaa8ff1bdf6a5b6636daf0f17f0e49cd17158
sya-ri <git@s7a.dev>
2022-07-12 20:43:25 +0900

    feat(analytics): Output result as CSV

--- packages/analytics/src/index.ts
+++ packages/analytics/src/index.ts
@@ -2,20 +2,23 @@ import Board from 'minesweeper-core/dist/Board'
 import RandomTilesGenerator from 'minesweeper-core/dist/generator/tile/RandomTilesGenerator'
 import TilesGenerator from 'minesweeper-core/dist/generator/tile/TilesGenerator'
 import RandomWithSimplexNoiseTilesGenerator from 'minesweeper-core/dist/generator/tile/RandomWithSimplexNoiseTilesGenerator'
+import * as fs from 'fs'
+import * as path from 'path'
 import Analyzer from './Analyzer'
+import AnalyzeResult from './AnalyzeResult'
 
-const analyze = (name: string, loopCount: number, tilesGenerator: () => TilesGenerator) => {
+const analyze = (
+  name: string,
+  loopCount: number,
+  tilesGenerator: () => TilesGenerator,
+  handleResult: (name: string, result: AnalyzeResult) => void
+) => {
   const analyzer = new Analyzer(() => {
     const board = new Board(tilesGenerator())
     board.openTile(Math.floor(board.width / 2), Math.floor(board.height / 2))
     return board
   })
-  const result = analyzer.runAverage(loopCount)
-  return {
-    name,
-    score: Analyzer.getScore(result),
-    ...result
-  }
+  handleResult(name, analyzer.runAverage(loopCount))
 }
 
 const run = async () => {
@@ -23,27 +26,32 @@ const run = async () => {
   const width = 30
   const height = 16
   const numberOfBomb = 99
-  const results = [
-    analyze('RandomTilesGenerator', loopCount, () => new RandomTilesGenerator(width, height, numberOfBomb, true))
-  ]
-  ;[0.05, 0.1, 0.5, 1, 5, 10, 15, 20].forEach((step) => {
-    ;[5, 10, 50, 100, 150, 200].forEach((numberOfBlanks) => {
-      results.push(
-        analyze(
-          `RandomWithSimplexNoiseTilesGenerator(${step}, ${numberOfBlanks})`,
-          loopCount,
-          () => new RandomWithSimplexNoiseTilesGenerator(width, height, numberOfBomb, step, numberOfBlanks, true)
-        )
+  const resultFilePath = path.join('dist', 'result.csv')
+  fs.writeFileSync(resultFilePath, `${['name', 'unopenedTiles', 'zeroBombTiles', 'eightBombTiles'].join()}\n`)
+  const handleResult = (name: string, result: AnalyzeResult) => {
+    const output = [
+      name,
+      result.unopenedTiles / result.tiles,
+      result.zeroBombTiles / result.tiles,
+      result.eightBombTiles / result.tiles
+    ].join()
+    fs.appendFileSync(resultFilePath, `${output}\n`)
+  }
+  analyze(
+    'RandomTilesGenerator',
+    loopCount,
+    () => new RandomTilesGenerator(width, height, numberOfBomb, true),
+    handleResult
+  )
+  ;[0.01, 0.05, 0.1, 0.5, 1, 5, 10, 15, 20, 25].forEach((step) => {
+    ;[5, 10, 25, 50, 100, 150, 200].forEach((numberOfBlanks) => {
+      analyze(
+        `RandomWithSimplexNoiseTilesGenerator(${step}:${numberOfBlanks})`,
+        loopCount,
+        () => new RandomWithSimplexNoiseTilesGenerator(width, height, numberOfBomb, step, numberOfBlanks, true),
+        handleResult
       )
     })
-  })
-  results.sort((a, b) => b.score - a.score)
-  results.forEach(({ name, score, tiles, unopenedTiles, zeroBombTiles, eightBombTiles }, index) => {
-    console.info(`${index + 1}. ${name}:`)
-    console.info(`  Score: ${score.toFixed(5)}`)
-    console.info(`  Unopened tiles: ${unopenedTiles} (${((unopenedTiles / tiles) * 100).toFixed(2)}%)`)
-    console.info(`  Tiles with no bombs around: ${zeroBombTiles}`)
-    console.info(`  Tiles with only bombs around: ${eightBombTiles}`)
   })
 }
 
