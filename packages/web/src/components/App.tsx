47b909bdeaa63549fae3881f73e823131c401455
sya-ri <git@s7a.dev>
2022-07-13 12:04:15 +0900

    refactor(web): Add BoardView

--- packages/web/src/components/App.tsx
+++ packages/web/src/components/App.tsx
@@ -1,40 +1,29 @@
 import React, { FC, useState } from 'react'
 import { Box, Button, Center, Flex } from '@chakra-ui/react'
-import { OpenTileResult } from 'minesweeper-core/dist/Tile'
+import { OpenTileResult, TilePosition } from 'minesweeper-core/dist/Tile'
 import Solver from 'minesweeper-core/dist/solver/Solver'
 import Board from 'minesweeper-core/dist/Board'
 import RandomTilesGenerator from 'minesweeper-core/dist/generator/tile/RandomTilesGenerator'
-import TileButton from './TileButton'
+import BoardView from './BoardView'
 
 const App: FC = () => {
   const [board, setBoard] = useState<Board>(new Board(new RandomTilesGenerator(30, 16, 99, true)))
+  const openTile = (tile: TilePosition) => {
+    const result = board.openTile(tile.x, tile.y)
+    if (result === OpenTileResult.GameOver) {
+      alert('Game Over') // eslint-disable-line no-alert
+      window.location.reload()
+    }
+    setBoard(board.clone())
+  }
+  const toggleFlag = (tile: TilePosition) => {
+    board.toggleFlag(tile.x, tile.y)
+    setBoard(board.clone())
+  }
   return (
     <Box m={2}>
       <Center overflow="auto">
-        <Box maxW="full">
-          {board.tiles.map((tt) => (
-            <Flex>
-              {tt.map((tile) => (
-                <TileButton
-                  tile={tile}
-                  countAroundBomb={() => board.countAroundBomb(tile.x, tile.y)}
-                  openTile={() => {
-                    const result = board.openTile(tile.x, tile.y)
-                    if (result === OpenTileResult.GameOver) {
-                      alert('Game Over') // eslint-disable-line no-alert
-                      window.location.reload()
-                    }
-                    setBoard(board.clone())
-                  }}
-                  toggleFlag={() => {
-                    board.toggleFlag(tile.x, tile.y)
-                    setBoard(board.clone())
-                  }}
-                />
-              ))}
-            </Flex>
-          ))}
-        </Box>
+        <BoardView board={board} openTile={openTile} toggleFlag={toggleFlag} />
       </Center>
       <Flex m={2} justify="center" gap={1}>
         <Button
---
+++ packages/web/src/components/BoardView.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from 'react'
+import { Box, Flex } from '@chakra-ui/react'
+import { OpenTileResult, TilePosition } from 'minesweeper-core/dist/Tile'
+import Board from 'minesweeper-core/dist/Board'
+import TileButton from './TileButton'
+
+export type BoardViewProps = {
+  board: Board
+  openTile: (tile: TilePosition) => void
+  toggleFlag: (tile: TilePosition) => void
+}
+
+const BoardView: FC<BoardViewProps> = ({ board, openTile, toggleFlag }) => {
+  return (
+    <Box maxW="full">
+      {board.tiles.map((tt) => (
+        <Flex>
+          {tt.map((tile) => (
+            <TileButton
+              tile={tile}
+              countAroundBomb={() => board.countAroundBomb(tile.x, tile.y)}
+              openTile={() => openTile(tile.position)}
+              toggleFlag={() => toggleFlag(tile.position)}
+            />
+          ))}
+        </Flex>
+      ))}
+    </Box>
+  )
+}
+
+export default BoardView
