7d70fd875191fee463a87532de3c3bbbb673764a
sya-ri <git@s7a.dev>
2022-07-20 11:09:04 +0900

    feat(web): Support RandomWithSimplexNoiseTilesGenerator

--- packages/web/src/components/App.tsx
+++ packages/web/src/components/App.tsx
@@ -1,14 +1,22 @@
 import React, { FC, useEffect, useState } from 'react'
-import { Box, Button, Center, Flex, useDisclosure } from '@chakra-ui/react'
+import { Box, Button, Center, Flex, NumberInput, NumberInputField, useDisclosure } from '@chakra-ui/react'
 import { TilePosition } from 'minesweeper-core/dist/Tile'
 import Solver from 'minesweeper-core/dist/solver/Solver'
 import Board from 'minesweeper-core/dist/Board'
 import RandomTilesGenerator from 'minesweeper-core/dist/generator/tile/RandomTilesGenerator'
+import RandomWithSimplexNoiseTilesGenerator from 'minesweeper-core/dist/generator/tile/RandomWithSimplexNoiseTilesGenerator'
 import BoardView from './BoardView'
 import GameOverModal from './GameOverModal'
 
 const App: FC = () => {
-  const generateTiles = () => new RandomTilesGenerator(30, 16, 99, true)
+  const [step, setStep] = useState(0.1)
+  const [blanks, setBlanks] = useState(100)
+  const generateTiles = () => {
+    if (step === 0 || blanks === 0) {
+      return new RandomTilesGenerator(30, 16, 99, true)
+    }
+    return new RandomWithSimplexNoiseTilesGenerator(30, 16, 99, step, blanks, true)
+  }
   const [board, setBoard] = useState(new Board(generateTiles()))
   const [isGameOver, setGameOver] = useState(false)
   const { isOpen: isOpenGameOverModal, onOpen: onOpenGameOverModal, onClose: onCloseGameOverModal } = useDisclosure()
@@ -34,6 +42,14 @@ const App: FC = () => {
       <Center overflow="auto">
         <BoardView board={board} openTile={openTile} toggleFlag={toggleFlag} isGameOver={isGameOver} />
       </Center>
+      <Flex m={2} justify="center" gap={1}>
+        <NumberInput defaultValue={step} onChange={(_value, value) => setStep(value)}>
+          <NumberInputField placeholder="Simplex Noise Step" />
+        </NumberInput>
+        <NumberInput defaultValue={blanks} onChange={(_value, value) => setBlanks(value)}>
+          <NumberInputField placeholder="Simplex Noise Blanks" />
+        </NumberInput>
+      </Flex>
       <Flex m={2} justify="center" gap={1}>
         <Button onClick={() => setBoard(new Board(generateTiles()))}>Regenerate</Button>
         <Button
