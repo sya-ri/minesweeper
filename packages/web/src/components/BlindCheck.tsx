552fad93d0924ffe72f4834d180157a56d4fac80
sya-ri <git@s7a.dev>
2022-10-11 05:21:24 +0900

    tweak(web): Change blind check params

--- packages/web/src/components/BlindCheck.tsx
+++ packages/web/src/components/BlindCheck.tsx
@@ -8,17 +8,14 @@ import GameOverModal from './GameOverModal'
 import BoardView from './BoardView'
 
 const BlindCheck: FC = () => {
-  const stepList = [0, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5]
-  const blankList = [10, 30, 50, 100, 150, 200]
-  const randomStep = () => stepList[Math.floor(Math.random() * stepList.length)]
+  const blankList = [0, 100, 150, 200]
   const randomBlanks = () => blankList[Math.floor(Math.random() * blankList.length)]
-  const [step, setStep] = useState(randomStep())
   const [blanks, setBlanks] = useState(randomBlanks())
   const generateTiles = () => {
-    if (step === 0 || blanks === 0) {
+    if (blanks === 0) {
       return new RandomTilesGenerator(30, 16, 99, true)
     }
-    return new RandomWithSimplexNoiseTilesGenerator(30, 16, 99, step, blanks, true)
+    return new RandomWithSimplexNoiseTilesGenerator(30, 16, 99, 0.1, blanks, true)
   }
   const [beginTime, setBeginTime] = useState<Date | null>(null)
   const [endTime, setEndTime] = useState<Date | null>(null)
@@ -57,7 +54,6 @@ const BlindCheck: FC = () => {
       <Flex m={2} justify="center" gap={1}>
         <Button
           onClick={() => {
-            setStep(randomStep())
             setBlanks(randomBlanks())
             setBeginTime(null)
             setEndTime(null)
@@ -74,9 +70,8 @@ const BlindCheck: FC = () => {
             <br />
             Time: {Math.floor(endTime.getTime() / 1000 - beginTime.getTime() / 1000)}
             <br />
-            Step: {step}
             <br />
-            Blanks: {step === 0 ? 0 : blanks}
+            Blanks: {blanks}
           </Text>
         </Center>
       )}
