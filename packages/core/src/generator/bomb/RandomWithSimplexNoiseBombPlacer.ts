c1261af869efda466af2bdf1e735ba5ab61f06b3
sya-ri <git@s7a.dev>
2023-01-17 09:54:03 +0900

    Revert "feat: Add output candidates"
    This reverts commit a5e01408f11605f0250f08f3dbb267466d77c2e8.

--- packages/core/src/generator/bomb/RandomWithSimplexNoiseBombPlacer.ts
+++ packages/core/src/generator/bomb/RandomWithSimplexNoiseBombPlacer.ts
@@ -23,9 +23,6 @@ export default class RandomWithSimplexNoiseBombPlacer extends RandomBombPlacer {
     }).flat()
     noises.sort(({ noise: noise1 }, { noise: noise2 }) => noise2 - noise1)
     candidates.push(...noises.slice(0, Math.floor(this.width * this.height * this.candidate)))
-    console.log(
-      `${this.width} × ${this.height} : ${this.candidate}(${Math.floor(this.width * this.height * this.candidate)})`
-    )
     super.init(blanks, candidates)
   }
 }
